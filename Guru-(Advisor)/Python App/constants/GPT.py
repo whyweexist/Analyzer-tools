a21289d0c6c9fb308af2a49f28fac406836875ff
whyweexist <167321903+whyweexist@users.noreply.github.com>
2024-08-20 15:42:37 +0530

    GPT.py

--- Guru-(Advisor)/Python App/constants/GPT.py
+++ Guru-(Advisor)/Python App/constants/GPT.py
@@ -1,2 +1,2 @@
-apiKey = "[ REDACTED ]"
+apiKey = "[ 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 ]"
 openaiModel = 'gpt-3.5-turbo'
